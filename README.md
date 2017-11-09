4f00be75590aa9703901675a5952ef33320bd40b
FDws <885374150@qq.com>
2017-11-09 23:12:26 +0800

    归并排序

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 #### [2.  动态规划实现0-1背包](https://github.com/FDws/Algorithm-Analysis/tree/master/src/knapsack)
 #### [2.  搜索法-回溯 -- n皇后问题](https://github.com/FDws/Algorithm-Analysis/tree/master/src/nking)
 #### [2.  搜索法-广度优先 -- 布线问题(迷宫最短路径)](https://github.com/FDws/Algorithm-Analysis/tree/master/src/routeProblem)
+#### [2.  排序](https://github.com/FDws/Algorithm-Analysis/tree/master/src/sort)
+        [1. 归并排序](https://github.com/FDws/Algorithm-Analysis/tree/master/src/sort/MergeSort)
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,44 @@
+package sort;
+
+import java.util.Arrays;
+
+public class MergeSort implements Msort {
+
+	@Override
+	public int[] sort(int[] array) {
+
+		if (array.length <= 1) {
+			return array;
+		}
+		int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+		int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+
+		return add(sort(left), sort(right));
+
+	}
+
+	public int[] add(int[] a, int[] b) {
+		int[] result = new int[a.length + b.length];
+		int ai = 0;
+		int bi = 0;
+		int index = 0;
+		while (index < result.length) {
+			if (ai < a.length && (bi == b.length || a[ai] < b[bi])) {
+				result[index++] = a[ai++];
+			} else {
+				result[index++] = b[bi++];
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 7, 2, 5, 1, 9, 2, 3, 0, 10 };
+		MergeSort m = new MergeSort();
+		for (int x : m.sort(a)) {
+			System.out.println(x);
+		}
+	}
+
+}
